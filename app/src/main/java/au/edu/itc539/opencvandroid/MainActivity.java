5879e80d7c687eb49e09263d9be3c859a3a0712b
duane <duane.mcmahon@telstra.com>
2017-11-28 21:20:44 +1100

    unable to flip textview in landscape mode - text is not visible. Otherwise text appearing in video streaming is good.

--- app/src/main/java/au/edu/itc539/opencvandroid/MainActivity.java
+++ app/src/main/java/au/edu/itc539/opencvandroid/MainActivity.java
@@ -1,19 +1,34 @@
 package au.edu.itc539.opencvandroid;
+
 import android.Manifest;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Display;
 import android.view.MenuItem;
+import android.view.Surface;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -24,12 +39,14 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 
 // OpenCV Classes
 
-public class MainActivity extends AppCompatActivity implements CvCameraViewListener2 {
+public class MainActivity extends AppCompatActivity implements CvCameraViewListener2, SensorEventListener {
 
     // Used for logging success or failure messages
     private static final String TAG = "OCVSample::Activity";
@@ -41,10 +58,19 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     private boolean mIsJavaCamera = true;
     private MenuItem mItemSwitchCamera = null;
 
+    TextView portrait_label, landscape_label;
 
     Mat mRgba;
     Mat mRgbaF;
     Mat mRgbaT;
+
+    private SensorManager mSensorManager;
+    private Sensor mRotationSensor;
+
+    Animation animation;
+
+    private static final int SENSOR_DELAY = 500 * 1000; // 500ms
+    private static final int FROM_RADS_TO_DEGS = -57;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -67,15 +93,21 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     };
 
     public MainActivity() {
+
         Log.i(TAG, "Instantiated new " + this.getClass());
+
+
     }
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         Log.i(TAG, "called onCreate");
+
         super.onCreate(savedInstanceState);
+
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         //getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         //Remove title bar
@@ -121,6 +153,21 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
 
         mOpenCvCameraView.setCvCameraViewListener(this);
+
+        portrait_label = findViewById(R.id.fruit_target_portrait);
+
+        landscape_label = findViewById(R.id.fruit_target_landscape);
+
+
+        // Get an instance of the SensorManager
+        try {
+            mSensorManager = (SensorManager) getSystemService(Activity.SENSOR_SERVICE);
+            mRotationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+            mSensorManager.registerListener(this, mRotationSensor, SENSOR_DELAY);
+        } catch (Exception e) {
+            Toast.makeText(this, "Hardware compatibility issue", Toast.LENGTH_LONG).show();
+        }
+
     }
 
     @Override
@@ -129,7 +176,12 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         super.onPause();
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
+
+        mSensorManager.unregisterListener(this);
+        super.onStop();
+
     }
+
 
     @Override
     public void onResume()
@@ -142,7 +194,71 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
             Log.d(TAG, "OpenCV library found inside package. Using it!");
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }
+
+        if (mSensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).size() != 0) {
+            Sensor s = mSensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
+            mSensorManager.registerListener(this, s, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
     }
+
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor == mRotationSensor) {
+            if (event.values.length > 4) {
+                float[] truncatedRotationVector = new float[4];
+                System.arraycopy(event.values, 0, truncatedRotationVector, 0, 4);
+                update(truncatedRotationVector);
+            } else {
+                update(event.values);
+            }
+        }
+
+    }
+
+
+    private void update(float[] vectors) {
+        float[] rotationMatrix = new float[9];
+        SensorManager.getRotationMatrixFromVector(rotationMatrix, vectors);
+        int worldAxisX = SensorManager.AXIS_X;
+        int worldAxisZ = SensorManager.AXIS_Z;
+        float[] adjustedRotationMatrix = new float[9];
+        SensorManager.remapCoordinateSystem(rotationMatrix, worldAxisX, worldAxisZ, adjustedRotationMatrix);
+        float[] orientation = new float[3];
+        SensorManager.getOrientation(adjustedRotationMatrix, orientation);
+        //    float pitch = orientation[1] * FROM_RADS_TO_DEGS;
+        float roll = orientation[2] * FROM_RADS_TO_DEGS;    // relevant
+// (roll >= -180 && roll <= -70) || (
+        if ((roll >= 70 && roll <= 180)) {
+
+            portrait_label.setVisibility(View.GONE);
+
+            landscape_label.setVisibility(View.VISIBLE);
+
+
+        } else if (roll >= -180 && roll <= -70) {
+
+            portrait_label.setVisibility(View.GONE);
+
+            landscape_label.setVisibility(View.GONE);
+
+        } else {
+
+            portrait_label.setVisibility(View.VISIBLE);
+
+            landscape_label.setVisibility(View.GONE);
+
+        }
+
+        //    ((TextView)findViewById(R.id.fruit_target)).setText("Roll: "+roll);
+
+        //  ((TextView) findViewById(R.id.fruit_target) ).setText(getResources().getConfiguration().orientation);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
 
     public void onDestroy() {
         super.onDestroy();
@@ -164,6 +280,7 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
 
         return inputFrame.rgba();
+
 
     }
 }
